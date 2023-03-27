0e13d1355fcc4fda89e84ef095aae3df7166af79
Everett Harris <79everettharris@gmail.com>
2023-03-27 12:05:05 -0400

    created class, added data notations to assist with migration

---
+++ Models/Vehicles.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Knowledge.Models
+{
+    public class Vehicles
+    {
+        [Key]
+        public int VId { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string VLicensePlate { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string VModel { get; set; }
+        [Column(TypeName = "int")]
+        public int VYear { get; set; }
+        [Column(TypeName = "varchar(30)")]
+        public string VEngType { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string VColor { get; set; }
+        [Column(TypeName = "int")]
+        public int VMileage { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string VMake { get; set; }
+        [Column(TypeName = "varchar(10)")]  
+        public string VBooked { get; set; }
+    }
+}
